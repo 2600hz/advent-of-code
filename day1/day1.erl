6976d90ebbcfa05f9e2b9540c5fdcdd0bded83c1
Tyler Kiziah <tyler@2600hz.com>
2019-12-08 13:54:07 -0500

    Refactor day1 problems with lessons learned so far

--- day1/day1.erl
+++ day1/day1.erl
@@ -1,35 +1,26 @@
 -module(day1).
--export([calc_fuel_for_modules/0, calc_total_fuel/0]).
+-export([test/0]).
 
-read_input(FileName) ->
-    {ok, Data} = file:read_file(FileName),
-    lists:delete(<<>>, binary:split(Data, [<<"\n">>], [global])).
+read_input(InputFile) ->
+    {'ok', IODevice} = file:open(InputFile, ['read']),
+    read_input(IODevice, file:read_line(IODevice), []).
 
-process_input(L) ->
-    [ list_to_integer(binary_to_list(X)) || X <- L ].
+read_input(IODevice, 'eof', Lines) ->
+    file:close(IODevice),
+    Lines;
+read_input(IODevice, {'ok', Line}, Lines) ->
+    read_input(IODevice, file:read_line(IODevice), [list_to_integer(Line -- "\n") | Lines]).
 
-calc_module_fuel(X) ->
-    X div 3 - 2.
+mass_fuel_req(Mass) ->
+    (Mass div 3) - 2.
 
-calc_fuel_for_fuel(X) ->
-    calc_fuel_for_fuel(X, 0).
+mass_and_fuel_req(Mass) ->
+    mass_and_fuel_req(mass_fuel_req(Mass), 0).
 
-calc_fuel_for_fuel(X, Total) ->
-    Fuel = X div 3 - 2,
-    case Fuel >= 0 of
-        true -> calc_fuel_for_fuel(Fuel, Total + Fuel);
-        false -> Total
-    end.
+mass_and_fuel_req(Mass, Total) when Mass =< 0 ->
+    Total;
+mass_and_fuel_req(Mass, Total) ->
+    mass_and_fuel_req(mass_fuel_req(Mass), Mass + Total).
 
-calc_total_fuel_for_module(X) ->
-    ModuleFuel = X div 3 - 2,
-    FuelForFuel = calc_fuel_for_fuel(ModuleFuel),
-    ModuleFuel + FuelForFuel.
-
-calc_fuel_for_modules() ->
-    Input = process_input(read_input("day1.input")),
-    lists:sum([ calc_module_fuel(X) || X <- Input]).
-
-calc_total_fuel() ->
-    Input = process_input(read_input("day1.input")),
-    lists:sum([ calc_total_fuel_for_module(X) || X <- Input]).
+test() ->
+    lists:sum([ mass_and_fuel_req(Fuel) || Fuel <- read_input("day1.input")]).
