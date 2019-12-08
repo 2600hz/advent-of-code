744c83a961f2ac404c16e184f024c41dcedb1465
Joris Tirado <joris@tirado.com>
2019-12-08 02:31:48 -0800

    Solve day 5 part 2 exercise

--- src/lib/intcodeInterpreter.js
+++ src/lib/intcodeInterpreter.js
@@ -1,19 +1,40 @@
-import {
-  sum,
-  multiply,
-  objectPropGetter,
-  pipe,
-  pipePromise,
-  promptQuestion,
-  unary
-} from '../utils.js';
+import { sum, multiply, pipe, pipePromise, promptQuestion, unary } from '../utils.js';
 
-const getOpcode = pipe(
+const promptForInput = pipePromise(
+  promptQuestion,
+  Number
+);
+
+const parseOpcode = pipe(
   String,
   a => a.slice(-2),
   Number
 );
 
+// :: Number -> Number
+const getOpcode = ({lead, spec}) => new Promise((resolve, reject) => {
+  const opcode = parseOpcode(lead);
+  return spec[opcode] ? resolve(opcode) : reject(new Error(`Unknown opcode ${opcode}`));
+});
+
+const getStoreByOpcode = ({intcode, pointer, opcode, spec}) => {
+  const {store} = spec[opcode];
+  return store ? intcode[sum(pointer, store)] : undefined;
+};
+
+const getOperationByOpcode = ({opcode, spec}) => {
+  const {operation} = spec[opcode];
+  return operation || (() => undefined);
+};
+
+const getNextPointerByOpcode = ({opcode, pointer, parameters, spec}) => {
+  const {inc} = spec[opcode];
+  if (typeof inc === 'number') return sum(pointer, inc);
+  if (typeof inc === 'function') return inc(pointer, ...parameters);
+  return pointer;
+};
+
+// :: Number -> [Number]
 const getModes = pipe(
   String,
   s => s.slice(0, -2),
@@ -22,66 +43,96 @@ const getModes = pipe(
   s => [...s].map(Number)
 );
 
-const getValueForMode = ({intcode, mode, value}) => ({
-  0: intcode[value],
-  1: value
+const getValueForMode = ({intcode, mode, current}) => ({
+  0: intcode[current],
+  1: current
 })[mode];
 
+// :: Object -> [Number]
 const getParameters = ({intcode, pointer, modes}) => modes.map((mode, index) => getValueForMode({
   intcode,
   mode,
-  value: intcode[pointer + (index + 1)]
+  current: intcode[sum(pointer, sum(index, 1))]
 }));
 
-const getStore = ({intcode, pointer, opcode}) => ({
-  1: intcode[pointer + 3],
-  2: intcode[pointer + 3],
-  3: intcode[pointer + 1]
-})[opcode];
+const performOperation = ({opcode, parameters, spec}) => new Promise(async (resolve, reject) => {
+  const operation = getOperationByOpcode({ opcode, spec });
+  let result;
+  try {
+    result = await operation(...parameters);
+  } catch (e) {
+    return reject(e);
+  }
+  return resolve(result);
+});
 
-const getAnswer = pipePromise(
-  promptQuestion,
-  Number
-);
-
-const operationsByOpcode = { 1: sum, 2: multiply, 3: getAnswer, 4: unary(console.log) };
-
-const getOperationByOpcode = objectPropGetter(operationsByOpcode);
-
-const getIncrementByOpcode = objectPropGetter({ 1: 4, 2: 4, 3: 2, 4: 2 });
-
-const resolveParametersForOpcode = ({opcode, parameters}) => ({
-  1: parameters,
-  2: parameters,
-  3: ['Enter input > '],
-  4: parameters
-})[opcode];
-
-const performOperation = ({opcode, parameters}) => new Promise(async (resolve, reject) =>
-  typeof operationsByOpcode[opcode] === 'function'
-    ? resolve(getOperationByOpcode(opcode)(...resolveParametersForOpcode({ opcode, parameters })))
-    : reject(new Error(`Unknown opcode ${opcode} `)));
-
-const parseInstruction = ({intcode, pointer}) => {
+const parseInstruction = async ({intcode, pointer, spec}) => {
   const lead = intcode[pointer];
-  const opcode = getOpcode(lead);
+  const opcode = await getOpcode({ lead, spec });
   const modes = getModes(lead);
   const parameters = getParameters({ intcode, pointer, modes });
-  const store = getStore({ intcode, pointer, opcode });
-  return { opcode, parameters, store };
+  const store = getStoreByOpcode({ intcode, pointer, opcode, spec });
+  const nextPointer = getNextPointerByOpcode({ opcode, pointer, parameters, spec });
+  return { opcode, parameters, store, nextPointer };
 };
 
 // :: [Number] -> Promise([Number])
 export default async originalIntcode => {
+  const spec = {
+    1: {
+      store: 3,
+      operation: sum,
+      inc: 4
+    },
+    2: {
+      store: 3,
+      operation: multiply,
+      inc: 4
+    },
+    3: {
+      store: 1,
+      operation: async () => await promptForInput('Enter input > '),
+      inc: 2
+    },
+    4: {
+      operation: unary(console.log),
+      inc: 2
+    },
+    5: {
+      inc: (pointer, first, second) => first !== 0 ? second : sum(pointer, 3)
+    },
+    6: {
+      inc: (pointer, first, second) => first === 0 ? second : sum(pointer, 3)
+    },
+    7: {
+      store: 3,
+      operation: (first, second) => first < second ? 1 : 0,
+      inc: 4
+    },
+    8: {
+      store: 3,
+      operation: (first, second) => first === second ? 1 : 0,
+      inc: 4
+    },
+    99: {
+      operation: () => null
+    }
+  };
   const intcode = [...originalIntcode];
   let pointer = 0;
+  let result;
 
   do {
-    const {opcode, parameters, store} = parseInstruction({ intcode, pointer });
-    const result = await performOperation({ opcode, parameters });
-    if (Number.isInteger(result)) intcode[store] = result;
-    pointer += getIncrementByOpcode(opcode);
-  } while (getOpcode(intcode[pointer]) !== 99);
+    const {
+      opcode,
+      parameters,
+      store,
+      nextPointer
+    } = await parseInstruction({ intcode, pointer, spec });
+    result = await performOperation({ opcode, parameters, spec });
+    if (Number.isInteger(result) && Number.isInteger(store)) intcode[store] = result;
+    pointer = nextPointer;
+  } while (result !== null);
 
   return intcode;
 };
