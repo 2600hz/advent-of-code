e351d92fe2a820f668091979e23e7461314c43a8
Clayton Finney <claytonfinney@berkeley.edu>
2021-12-03 14:43:58 -0800

    code golfed (sorta)

--- day3/sol.py
+++ day3/sol.py
@@ -1,62 +1,10 @@
-def sol1(in_array):
-    gamma_str = ""
-    epsilon_str = ""
-    for i in range(len(in_array[0]) - 1):
-        ones = 0
-        zeroes = 0
-        for line in in_array:
-            if line[i] == "1":
-                ones += 1
-            else:
-                zeroes += 1
-        if ones > zeroes:
-            gamma_str += "1"
-            epsilon_str += "0"
-        else:
-            gamma_str += "0"
-            epsilon_str += "1"
-    
-    return int(gamma_str, 2) * int(epsilon_str, 2)
-
-
-def sol2(in_array):
-    oxy_list = in_array
-    co2_list = in_array
-    for i in range(len(oxy_list[0]) - 1):
-        ones = 0
-        zeroes = 0
-        for line in oxy_list:
-            if line[i] == "1":
-                ones += 1
-            else:
-                zeroes += 1
-        if ones >= zeroes:
-            if len(oxy_list) > 1:
-                oxy_list = list(filter(lambda x: x[i] == "1", oxy_list))
-        else:
-            if len(oxy_list) > 1:
-                oxy_list = list(filter(lambda x: x[i] == "0", oxy_list))
-
-    for i in range(len(co2_list[0]) - 1):
-        ones = 0
-        zeroes = 0
-        for line in co2_list:
-            if line[i] == "1":
-                ones += 1
-            else:
-                zeroes += 1
-        if ones >= zeroes:
-            if len(co2_list) > 1:
-                co2_list = list(filter(lambda x: x[i] == "0", co2_list))
-        else:
-            if len(co2_list) > 1:
-                co2_list = list(filter(lambda x: x[i] == "1", co2_list))
-    
-    return int(oxy_list[0], 2) * int(co2_list[0], 2)
-
-
-with open('input.txt', 'r') as r:
-    in_array = r.readlines()
-
-print(sol1(in_array))
-print(sol2(in_array))
+#pt1
+with open('input.txt', 'r') as r:p = r.readlines();a = [[line[i] for line in p] for i in range(len(p[0]) - 1)];print(int("".join(["1" if char_list.count("1") >= char_list.count("0") else "0" for char_list in a]), 2) * int("".join(["0" if char_list.count("1") >= char_list.count("0") else "1" for char_list in a]), 2))
+#pt2
+curr=0
+while len(oxy_r)>1:
+    a = [[line[i] for line in oxy_r] for i in range(len(oxy_r[0]) - 1)];oxy_r = list(filter(lambda x: x[curr] == "1", oxy_r)) if a[curr].count("1") >= a[curr].count("0") else list(filter(lambda x: x[curr] == "0", oxy_r));curr+=1
+curr=0
+while len(co2_r)>1:
+    a = [[line[i] for line in co2_r] for i in range(len(co2_r[0]) - 1)];co2_r = list(filter(lambda x: x[curr] == "0", co2_r)) if a[curr].count("1") >= a[curr].count("0") else list(filter(lambda x: x[curr] == "1", co2_r));curr+=1
+print(int(oxy_r[0], 2) * int(co2_r[0], 2))
