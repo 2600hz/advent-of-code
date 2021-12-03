9983906893b79b3672a3f5976adaf11d30411d64
Clayton Finney <claytonfinney@berkeley.edu>
2021-12-03 14:49:41 -0800

    small error

--- day3/sol.py
+++ day3/sol.py
@@ -1,7 +1,7 @@
 #pt1
 with open('input.txt', 'r') as r:p = r.readlines();a = [[line[i] for line in p] for i in range(len(p[0]) - 1)];print(int("".join(["1" if char_list.count("1") >= char_list.count("0") else "0" for char_list in a]), 2) * int("".join(["0" if char_list.count("1") >= char_list.count("0") else "1" for char_list in a]), 2))
 #pt2
-curr=0
+oxy_r,co2_r,curr=p,p,0
 while len(oxy_r)>1:
     a = [[line[i] for line in oxy_r] for i in range(len(oxy_r[0]) - 1)];oxy_r = list(filter(lambda x: x[curr] == "1", oxy_r)) if a[curr].count("1") >= a[curr].count("0") else list(filter(lambda x: x[curr] == "0", oxy_r));curr+=1
 curr=0
