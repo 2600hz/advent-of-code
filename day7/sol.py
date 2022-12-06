fbf6481dfd11a482b379304efc008ea7f70ea934
Clayton Finney <claytonfinney@berkeley.edu>
2022-12-06 13:57:29 -0800

    day6 done, day 7 skeleton created hehe

---
+++ day7/sol.py
@@ -0,0 +1,12 @@
+def part_one(lines):
+    return 0
+
+def part_two(lines):
+    return 0
+
+with open("input.txt", "r") as r:
+    raw_lines = r.readlines()
+
+
+print("part one solutions is {}".format(part_one(raw_lines)))
+print("part two solutions is {}".format(part_two(raw_lines)))
