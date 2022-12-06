7829437c72d7de37a463d7235597b5f7872d77eb
Clayton Finney <claytonfinney@berkeley.edu>
2022-12-06 14:26:28 -0800

    generalized function for day6

--- day6/sol.py
+++ day6/sol.py
@@ -1,15 +1,10 @@
-def part_one(line):
-    for i in range(3, len(line)):
-        if len(set(line[i-3:i+1])) == 4:
-            return i+1
-
-def part_two(line):
-    for i in range(13, len(line)):
-        if len(set(line[i-13:i+1])) == 14:
+def sol(line, offset):
+    for i in range(offset-1, len(line)):
+        if len(set(line[i-(offset-1):i+1])) == offset:
             return i+1
 
 with open("input.txt", "r") as r:
     raw_line = r.readlines()[0]
 
-print("part one solutions is {}".format(part_one(raw_line)))
-print("part two solutions is {}".format(part_two(raw_line)))
+print("part one solutions is {}".format(sol(raw_line, 4)))
+print("part two solutions is {}".format(sol(raw_line, 14)))
