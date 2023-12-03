95559c5bfbbeb2e2395d5c323a3c288cd16c1050
Guillermo Gutiérrez <guillegr123@gmail.com>
2023-12-03 11:42:09 -0600

    Day 3, part 2

--- advent-of-code-2023/src/main/scala/Day3.scala
+++ advent-of-code-2023/src/main/scala/Day3.scala
@@ -51,4 +51,46 @@ object Day3 {
 
     Await.result(future, Duration.Inf)
   }
+
+  def runPart2 = {
+    val cogRegex = "\\*".r
+    val numberRegex = "\\d+".r
+
+    val fs = FileSystems.getDefault
+
+    val sink = Sink.fold[Int, Int](0)(_ + _)
+
+    val extraLineSource = Source(List(""))
+
+    val future = FileIO.fromPath(fs.getPath("../day3/input.txt"))
+      .via(Framing.delimiter(ByteString("\n"), 256, true))
+      .map(_.utf8String)
+      .prepend(extraLineSource)
+      .concat(extraLineSource)
+      .sliding(3)
+      .flatMapConcat(lines => {
+        val numberMatches = lines.flatMap(numberRegex.findAllMatchIn(_).toList).toList
+        val cogMatches = cogRegex.findAllMatchIn(lines(1)).toList
+
+        val validNumberMatches = cogMatches
+          .map(cogMatch => {
+            numberMatches.filter(numberMatch => {
+              val fromIndex = numberMatch.start - 1
+              val toIndex = numberMatch.end
+              cogMatch.start >= fromIndex && cogMatch.start <= toIndex
+            })
+          })
+          .filter(numberMatches => numberMatches.length == 2)
+          .map(numberMatches => numberMatches.map(_.matched.toInt).fold(1)(_ * _))
+
+        Source(validNumberMatches)
+      })
+      /*.map(x => {
+        println(x)
+        x
+      })*/
+      .runWith(sink)
+
+    Await.result(future, Duration.Inf)
+  }
 }
