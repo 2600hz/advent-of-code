415375a3c775db8bf635a01533334ad85acfc779
James Aimonetti <james@2600hz.com>
2023-12-06 16:21:04 -0800

    day5 part 2 incomplete

--- src/day05.erl
+++ src/day05.erl
@@ -2,192 +2,43 @@
 
 -export([run/0]).
 
-%% --- Day 5: If You Give A Seed A Fertilizer ---
-
-%% You take the boat and find the gardener right where you were told
-%% he would be: managing a giant "garden" that looks more to you like
-%% a farm.
-
-%% "A water source? Island Island is the water source!" You point out
-%% that Snow Island isn't receiving any water.
-
-%% "Oh, we had to stop the water because we ran out of sand to filter
-%% it with! Can't make snow with dirty water. Don't worry, I'm sure
-%% we'll get more sand soon; we only turned off the water a few
-%% days... weeks... oh no." His face sinks into a look of horrified
-%% realization.
-
-%% "I've been so busy making sure everyone here has food that I
-%% completely forgot to check why we stopped getting more sand!
-%% There's a ferry leaving soon that is headed over in that direction
-%% - it's much faster than your boat. Could you please go check it
-%% out?"
-
-%% You barely have time to agree to this request when he brings up
-%% another. "While you wait for the ferry, maybe you can help us with
-%% our food production problem. The latest Island Island Almanac just
-%% arrived and we're having trouble making sense of it."
-
-%% The almanac (your puzzle input) lists all of the seeds that need to
-%% be planted. It also lists what type of soil to use with each kind
-%% of seed, what type of fertilizer to use with each kind of soil,
-%% what type of water to use with each kind of fertilizer, and so
-%% on. Every type of seed, soil, fertilizer and so on is identified
-%% with a number, but numbers are reused by each category - that is,
-%% soil 123 and fertilizer 123 aren't necessarily related to each
-%% other.
-
-%% For example:
-
-%% seeds: 79 14 55 13
-
-%% seed-to-soil map:
-%% 50 98 2
-%% 52 50 48
-
-%% soil-to-fertilizer map:
-%% 0 15 37
-%% 37 52 2
-%% 39 0 15
-
-%% fertilizer-to-water map:
-%% 49 53 8
-%% 0 11 42
-%% 42 0 7
-%% 57 7 4
-
-%% water-to-light map:
-%% 88 18 7
-%% 18 25 70
-
-%% light-to-temperature map:
-%% 45 77 23
-%% 81 45 19
-%% 68 64 13
-
-%% temperature-to-humidity map:
-%% 0 69 1
-%% 1 0 69
-
-%% humidity-to-location map:
-%% 60 56 37
-%% 56 93 4
-
-%% The almanac starts by listing which seeds need to be planted: seeds
-%% 79, 14, 55, and 13.
-
-%% The rest of the almanac contains a list of maps which describe how
-%% to convert numbers from a source category into numbers in a
-%% destination category. That is, the section that starts with
-%% seed-to-soil map: describes how to convert a seed number (the
-%% source) to a soil number (the destination). This lets the gardener
-%% and his team know which soil to use with which seeds, which water
-%% to use with which fertilizer, and so on.
-
-%% Rather than list every source number and its corresponding
-%% destination number one by one, the maps describe entire ranges of
-%% numbers that can be converted. Each line within a map contains
-%% three numbers: the destination range start, the source range start,
-%% and the range length.
-
-%% Consider again the example seed-to-soil map:
-
-%% 50 98 2
-%% 52 50 48
-
-%% The first line has a destination range start of 50, a source range
-%% start of 98, and a range length of 2. This line means that the
-%% source range starts at 98 and contains two values: 98 and 99. The
-%% destination range is the same length, but it starts at 50, so its
-%% two values are 50 and 51. With this information, you know that seed
-%% number 98 corresponds to soil number 50 and that seed number 99
-%% corresponds to soil number 51.
-
-%% The second line means that the source range starts at 50 and
-%% contains 48 values: 50, 51, ..., 96, 97. This corresponds to a
-%% destination range starting at 52 and also containing 48 values: 52,
-%% 53, ..., 98, 99. So, seed number 53 corresponds to soil number 55.
-
-%% Any source numbers that aren't mapped correspond to the same
-%% destination number. So, seed number 10 corresponds to soil number
-%% 10.
-
-%% So, the entire list of seed numbers and their corresponding soil
-%% numbers looks like this:
-
-%% seed  soil
-%% 0     0
-%% 1     1
-%% ...   ...
-%% 48    48
-%% 49    49
-%% 50    52
-%% 51    53
-%% ...   ...
-%% 96    98
-%% 97    99
-%% 98    50
-%% 99    51
-
-%% With this map, you can look up the soil number required for each
-%% initial seed number:
-
-%%     Seed number 79 corresponds to soil number 81.
-%%     Seed number 14 corresponds to soil number 14.
-%%     Seed number 55 corresponds to soil number 57.
-%%     Seed number 13 corresponds to soil number 13.
-
-%% The gardener and his team want to get started as soon as possible,
-%% so they'd like to know the closest location that needs a
-%% seed. Using these maps, find the lowest location number that
-%% corresponds to any of the initial seeds. To do this, you'll need to
-%% convert each seed number through other categories until you can
-%% find its corresponding location number. In this example, the
-%% corresponding types are:
-
-%%     Seed 79, soil 81, fertilizer 81, water 81, light 74,
-%%     temperature 78, humidity 78, location 82.
-%%     Seed 14, soil 14, fertilizer 53, water 49, light 42,
-%%     temperature 42, humidity 43, location 43.
-%%     Seed 55, soil 57, fertilizer 57, water 53, light 46,
-%%     temperature 82, humidity 82, location 86.
-%%     Seed 13, soil 13, fertilizer 52, water 41, light 34,
-%%     temperature 34, humidity 35, location 35.
-
-%% So, the lowest location number in this example is 35.
-
-%% What is the lowest location number that corresponds to any of the
-%% initial seed numbers?
-
-
+%% https://adventofcode.com/2023/day/5
 %% lowest location: 382895070
 
 run() ->
-    part1(input("day05_part1.txt")),
-    part2(input("day05_part1.txt")).
+    io:format("starting~n"),
+    Almanac = parse_almanac(input("day05_part1.txt")),
+    io:format('user', "almanac: ~p~n", [Almanac]),
 
-part1(Input) ->
-    Almanac = parse_almanac(Input),
-    Locations = seeds_to_locations(Almanac),
-    io:format("~nlowest location: ~w~n", [lists:min(Locations)]).
+    part1(Almanac),
+    part2(Almanac).
+
+part1({Seeds, Maps}) ->
+    SeedRanges = [{Seed, Seed} || Seed <- Seeds],
+    Locations = seeds_to_locations({SeedRanges, Maps}),
+    io:format('user', "~nlowest location: ~w~n", [lists:min(Locations)]).
 
 seeds_to_locations({Seeds, Maps}) ->
     {_, Locations} = lists:foldl(fun seed_to_location/2, {Maps, []}, Seeds),
     lists:reverse(Locations).
 
-seed_to_location(Seed, {Maps, Locations}) ->
+seed_to_location({SeedStart, SeedStart}, {Maps, Locations}) ->
     SrcName = <<"seed">>,
     FinalDstName = <<"location">>,
 
-    Location = seed_to_location(Seed, Maps, SrcName, FinalDstName),
-    {Maps, [Location | Locations]}.
+    Location = seed_to_location(SeedStart, Maps, SrcName, FinalDstName),
+    {Maps, [Location | Locations]};
+seed_to_location({SeedStart, SeedEnd}, {Maps, Locations}) ->
+    {Maps, Ls} = seed_to_location({SeedStart, SeedStart}, {Maps, Locations}),
+    seed_to_location({SeedStart+1, SeedEnd}, {Maps, Ls}).
 
 seed_to_location(Seed, Maps, SrcName, FinalDstName) ->
-    seed_to_location(Seed, Maps, SrcName, FinalDstName, maps:get(SrcName, Maps)).
+    io:format('user', "Finding ~s~n", [SrcName]),
+    seed_to_location(Seed, Maps, SrcName, FinalDstName, lists:keyfind(SrcName, 1, Maps)).
 
-seed_to_location(Seed, _Maps, _SrcName, FinalDstName, {FinalDstName, Ranges}) ->
+seed_to_location(Seed, _Maps, SrcName, FinalDstName, {SrcName, FinalDstName, Ranges}) ->
     map_src_to_dst(Seed, lists:reverse(Ranges));
-seed_to_location(Seed, Maps, _SrcName, FinalDstName, {DstName, Ranges}) ->
+seed_to_location(Seed, Maps, SrcName, FinalDstName, {SrcName, DstName, Ranges}) ->
     NewSeed = map_src_to_dst(Seed, lists:reverse(Ranges)),
     seed_to_location(NewSeed, Maps, DstName, FinalDstName).
 
@@ -209,41 +60,94 @@ parse_almanac(Input) ->
     {SeedNos, Maps}.
 
 parse_maps(MapBins) ->
-    parse_maps(MapBins, #{}).
+    parse_maps(MapBins, []).
 
 parse_maps([], Maps) -> Maps;
 parse_maps([<<>>, MapNameBin | MapBins], Maps) ->
+    parse_maps([MapNameBin | MapBins], Maps);
+parse_maps([MapNameBin | MapBins], Maps) ->
     {'match', [SrcName, DstName]}
         = re:run(MapNameBin
                 ,<<"^(\\w+)-to-(\\w+) map:$">>
                 ,[{'capture', 'all_but_first', 'binary'}]
                 ),
-    %% {srcname => {dstname, [range()]}
-    parse_map(MapBins, Maps#{SrcName => {DstName, []}}, SrcName).
+    %% [{srcname, dstname, [range()]}]
+    {MapRanges, MapBins1} = parse_map_ranges(MapBins, []),
+    parse_maps(MapBins1, [{SrcName, DstName, MapRanges} | Maps]).
 
-parse_map([], Maps, _SrcName) -> Maps;
-parse_map([<<>> | _]=MapBins, Maps, _SrcName) ->
-    parse_maps(MapBins, Maps);
-parse_map([SrcDst | MapBins], Maps, SrcName) ->
+parse_map_ranges([], Ranges) -> {Ranges, []};
+parse_map_ranges([<<>> | MapBins], Ranges) ->
+    {Ranges, MapBins};
+parse_map_ranges([SrcDst | MapBins], Ranges) ->
     [DstStartBin, SrcStartBin, RangeBin] = binary:split(SrcDst, <<" ">>, ['global', 'trim']),
 
     DstStart = binary_to_integer(DstStartBin),
     SrcStart = binary_to_integer(SrcStartBin),
     Range = binary_to_integer(RangeBin),
 
-    parse_map(MapBins
-             ,maps:update_with(SrcName
-                              ,fun({DstName, Ranges}) ->
-                                       {DstName, [{DstStart, SrcStart, Range} | Ranges]}
-                               end
-                              ,Maps
-                              )
-             ,SrcName
-             ).
+    parse_map_ranges(MapBins
+                    ,[{DstStart, SrcStart, Range} | Ranges]
+                    ).
+
+part2({Seeds, Maps}) ->
+    SeedRanges = parse_seed_ranges(Seeds),
+    Collapsed = collapse_seed_ranges(SeedRanges),
+
+    Locations = find_locations(Collapsed, Maps, []),
+
+    io:format("~nlowest location: ~w~n", [lists:min(Locations)]).
+
+find_locations([], _Maps, Locations) -> Locations;
+find_locations([Seed, Range | Seeds], [{_SrcName, _DstName, Ranges} | Maps], Locations) ->
+    {Ls, SeedRanges} = find_location(Seed, Seed+Range, Ranges, {Locations, []}),
+    find_locations(Seeds ++ SeedRanges, Maps, Ls).
+
+find_location(SeedStart, SeedEnd, [{DstStart, SrcStart, Range} | Maps], {Locations, MoreSeedRanges})
+  when SeedStart >= SrcStart andalso SeedEnd =< SrcStart+Range ->
+    %% all seed range are mapped to the map's range
+    Offset = DstStart - SrcStart,
+    find_location(SeedStart + Offset
+                 ,SeedEnd + Offset
+                 ,Maps
+                 ,{Locations, MoreSeedRanges}
+                 );
+find_location(SeedStart, SeedEnd, [{DstStart, SrcStart, _Range} | Maps], {Locations, MoreSeedRanges})
+  when SeedStart < SrcStart andalso SeedEnd > SrcStart ->
+    %% seed range
+    {Locations, MoreSeedRanges}.
 
 
-part2(Input) ->
-    Input.
+
+collapse_seed_ranges(SeedRanges) ->
+    lists:foldl(fun collapse_seed_range/2, [], SeedRanges).
+
+collapse_seed_range({Start, End}, [{RangeStart, RangeEnd} | Ranges])
+  when Start < RangeEnd ->
+    %% when Start is within existing range, replace range with new End
+    [{RangeStart, End} | Ranges];
+collapse_seed_range({Start, End}, Ranges) ->
+    [{Start, End} | Ranges].
+
+parse_seed_ranges(Seeds) ->
+    parse_seed_ranges(Seeds, []).
+
+parse_seed_ranges([], Ranges) -> lists:keysort(1, Ranges);
+parse_seed_ranges([SeedNo, Range | SeedRanges], Ranges) ->
+    parse_seed_ranges(SeedRanges, seed_range(SeedNo, Range, Ranges, Ranges)).
+
+seed_range(SeedNo, Range, ExistingSeedRanges, []) ->
+    [{SeedNo, SeedNo+Range} | ExistingSeedRanges];
+seed_range(SeedNo, Range, ExistingSeedRanges, [{Start, End} | _SeedRanges])
+  when SeedNo >= Start andalso SeedNo+Range =< End ->
+    %% already have a range covering this seed
+    ExistingSeedRanges;
+seed_range(SeedNo, Range, ExistingSeedRanges, [{Start, End} | _SeedRanges])
+  when SeedNo < Start andalso SeedNo+Range > End  ->
+    %% new range covers existing range
+    [{SeedNo, SeedNo+Range} | lists:keydelete(Start, 1, ExistingSeedRanges)];
+seed_range(SeedNo, Range, ExistingSeedRanges, [{_Start, _End} | SeedRanges]) ->
+    seed_range(SeedNo, Range, ExistingSeedRanges, SeedRanges).
+
 
 input(File) ->
     {'ok', Bin} = file:read_file(filename:join(["src", File])),
