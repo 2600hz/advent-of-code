292dc517b7c40793ac50a1e556aa5e652fb32bcd
James Aimonetti <james@2600hz.com>
2023-12-10 10:27:26 -0800

    unused

--- src/day08.erl
+++ src/day08.erl
@@ -52,10 +52,6 @@ gcd(A, B) when A < 0 orelse B < 0 -> gcd(abs(A), abs(B));
 gcd(A, B) when A < B -> gcd(B, A);
 gcd(A, B) -> gcd(B, A - B * (A div B)).
 
-%% gcd(A, B, C) = gcd( gcd(a,b), c)
-gcd([A, B | L]) ->
-    lists:foldl(fun(X, Acc) -> gcd(X, Acc) end, gcd(A, B), L).
-
 lcm(A, B) ->
     A * B div gcd(A, B).
 %% lcm(A, B, C) = lcm( lcm(a,b), c)
