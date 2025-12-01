e3e0ddc84ddbc3502e9f660320e63eeee5207373
James Aimonetti <james@2600hz.com>
2025-11-30 22:20:36 -0800

    Day1 part 2

--- aoc/src/aoc.erl
+++ aoc/src/aoc.erl
@@ -7,7 +7,10 @@ day1(InputType) ->
     Rotations = parse_rotations(Lines),
     StartPoint = 50,
     {Zeros, EndPoint} = lists:foldl(fun rotate_dial/2, {0, StartPoint}, Rotations),
-    io:format("ended at point ~p, hit ~p zeros~n", [EndPoint, Zeros]).
+
+    {SecureZeros, EndPoint} = lists:foldl(fun rotate_dial_0x434C49434B/2, {0, StartPoint}, Rotations),
+    io:format("part1:~n  ended at point ~p, hit ~p zeros~n", [EndPoint, Zeros]),
+    io:format("part2:~n  ended at point ~p, passed ~p zeros~n", [EndPoint, SecureZeros]).
 
 rotate_dial({Op, Turns}, {Zeros, Point}) ->
     case normalize_point(Op(Point, Turns)) of
@@ -21,6 +24,25 @@ normalize_point(Negative) when Negative < 0 ->
 normalize_point(Over99) when Over99 > 99 ->
     normalize_point(Over99 - 100);
 normalize_point(Point) -> Point.
+
+rotate_dial_0x434C49434B({Op, Turns}, {Zeros, Point}) ->
+    {NewPoint, Passes} = normalize_point_0x434C49434B(Point, Op(Point, Turns)),
+    {Zeros + Passes, NewPoint}.
+
+normalize_point_0x434C49434B(Point, NewPoint) ->
+    normalize_point_0x434C49434B(Point, NewPoint, 0).
+
+normalize_point_0x434C49434B(_Point, 0, Passes) ->
+    {0, Passes+1};
+normalize_point_0x434C49434B(_Point, 100, Passes) ->
+    {0, Passes+1};
+normalize_point_0x434C49434B(0, Negative, Passes) when Negative < 0 ->
+    normalize_point_0x434C49434B(Negative, Negative+100, Passes);
+normalize_point_0x434C49434B(_Point, Negative, Passes) when Negative < 0 ->
+    normalize_point_0x434C49434B(Negative, Negative+100, Passes+1);
+normalize_point_0x434C49434B(_Point, Over99, Passes) when Over99 > 99 ->
+    normalize_point_0x434C49434B(Over99, Over99 - 100, Passes+1);
+normalize_point_0x434C49434B(_Point, NewPoint, Passes) -> {NewPoint, Passes}.
 
 parse_rotations(Lines) ->
     [parse_rotation(Line) || Line <- Lines].
